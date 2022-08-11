09d3e39efc335b52ca4a6d0494f06dcd161514c9
Zvezegor <ferocaritas@gmail.com>
2022-08-11 18:59:44 +0300

    completed

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", function (event) {
+  modal.classList.add("active");
+});
+close.addEventListener("click", function (event) {
+  modal.classList.remove("active");
+});
